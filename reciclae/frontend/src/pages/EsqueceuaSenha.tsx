a4557c354acf4fd4b7f72194d7a72a249a9a87e7
Juan Silva Souza <juansilvasouza@icloud.com>
2025-11-10 23:24:08 -0200

    git commit -m "[Juan] "add uma page para o esqueceu a senha"

---
+++ reciclae/frontend/src/pages/EsqueceuaSenha.tsx
@@ -0,0 +1,21 @@
+//import React from 'react'
+import PageTitle from '../components/layout/PageTitle'
+import Section from '../components/layout/Section'
+import DetailView from '../components/data-display/DetailView'
+import Skeleton from '../components/loading/Skeleton'
+import EmptyState from '../components/empty/EmptyState'
+
+export default function EsqueceuaSenha() {
+  return (
+    <>
+     
+
+
+
+
+
+
+     
+    </>
+  )
+}
