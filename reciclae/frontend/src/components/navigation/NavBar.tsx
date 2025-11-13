35b231a7c1a484afa319f31e83658577821539a9
Mech24689 <gabrieltrem102030@gmail.com>
2025-11-13 20:34:51 -0300

    [Gabriel Castro] construção de rotas

--- reciclae/frontend/src/components/navigation/NavBar.tsx
+++ reciclae/frontend/src/components/navigation/NavBar.tsx
@@ -31,18 +31,38 @@ export default function NavBar() {
 
   // Função para navegar para a rota de Cadastro
   const handleCadastroClick = () => {
-    navigate('/cadastro'); // Rota de destino para Cadastro
+    navigate('/cadastro-user'); // Rota de destino para Cadastro
+  }
+
+  const handleAboutClick = () => {
+    navigate('/sobre-nos'); // Rota de destino para Cadastro
+  }
+
+  const handleDiasColetaClick = () => {
+    navigate('/dias-coleta'); // Rota de destino para Cadastro
+  }
+
+  const handlePontosColetaClick = () => {
+    navigate('/pontos-coleta'); // Rota de destino para Cadastro
+  }
+
+  const handleCadastroVeiculoClick = () => {
+    navigate('/cadastro-veiculo'); // Rota de destino para Cadastro
+  }
+
+  const handleContatoClick = () => {
+    navigate('/contato'); // Rota de destino para Cadastro
   }
 
   return (
     <nav className='btn-header'>
       <ul>
         {/* Você pode substituir <li> por <Link> se forem links simples */}
-        <li className='btn-header-link'>Sobre nós</li>
-        <li className='btn-header-link'>Dias de coleta</li>
-        <li className='btn-header-link'>Pontos de coleta</li>
-        <li className='btn-header-link'>Cadastrar Veículo</li>
-        <li className='btn-header-link'>Contato</li>
+        <li className='btn-header-link' onClick={handleAboutClick}>Sobre nós</li>
+        <li className='btn-header-link' onClick={handleDiasColetaClick}>Dias de coleta</li>
+        <li className='btn-header-link' onClick={handlePontosColetaClick}>Pontos de coleta</li>
+        <li className='btn-header-link' onClick={handleCadastroVeiculoClick}>Cadastrar Veículo</li>
+        <li className='btn-header-link' onClick={handleContatoClick}>Contato</li>
         
         {/* Botão de Login com navegação */}
         <li 
